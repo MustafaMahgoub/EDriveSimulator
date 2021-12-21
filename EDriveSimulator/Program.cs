db791198328b122ed1dea27fee7e728d5ebe2e0d
Mostafa Mahgoub <mostafa.mahgoub@belgiantrain.be>
2021-12-21 22:52:25 +0100

    Initial add

---
+++ EDriveSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EDriveSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
